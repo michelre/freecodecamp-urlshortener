8b8061fa00ce3ce94d9c6bedfa6079ccf9c76767
Rémi Michel <remi.michel@kaizen-solutions.net>
2016-07-08 15:19:54 +0200

    initial commit

---
+++ mongo-utils.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/surl';
+
+module.exports = {
+  insert: (data) => new Promise((resolve, reject) => {
+    MongoClient.connect(url, function(err, db) {
+      const collection = db.collection('urls');
+      collection.insert(data)
+        .then(res => {
+          db.close();
+          resolve(res)
+        }, err => {
+          db.close();
+          reject(err);
+        });
+    });
+  }),
+  get: (id) => new Promise((resolve, reject) => {
+    MongoClient.connect(url, function (err, db) {
+      const collection = db.collection('urls');
+      collection.find({ id }, { _id: 0 })
+        .toArray((err, urls) => {
+          db.close();
+          resolve(urls[0])
+        });
+    });
+  })
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const utils = require('./utils');
+const mongoUtils = require('./mongo-utils');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser());
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send({status: 'OK'});
+});
+
+app.get('/shorten', (req, res) => {
+  const id = utils.generateId();
+  const baseUrl = req.query.url || 'http://www.google.com';
+  const re = /^http:\/\/www.[a-zA-Z]+.[a-zA-Z]+/i;
+  if(!re.test(baseUrl)) {
+    res.send({ error: 'Your url is not valid' });
+  }
+  else {
+    mongoUtils
+      .insert({url: baseUrl, id})
+      .then(() => res.send({ url: baseUrl, short_url: `http://${req.get('host')}/${id}` }))
+      .catch(err => res.status(500).send(err));
+  }
+});
+
+app.get('/:id', (req, res) => {
+  mongoUtils
+    .get(req.params.id)
+    .then((d) => res.redirect(d.url))
+    .catch(err => res.status(500).send(err));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
